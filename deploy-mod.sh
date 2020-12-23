ba4e18631ba4dae5bc6e55a59136308bee353004
falkTX <falktx@falktx.com>
2020-12-23 11:27:29 +0000

    Add script that deploys to a MOD dwarf

---
+++ deploy-mod.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd $(dirname "${0}")
+
+ssh root@moddwarf.local "mount / -o remount,rw"
+ssh root@moddwarf.local "rm -f /usr/lib/python3.4/site-packages/browsepy/*.pyc"
+
+scp browsepy/*.py root@moddwarf.local:/usr/lib/python3.4/site-packages/browsepy/
+scp browsepy/static/base.css root@moddwarf.local:/usr/lib/python3.4/site-packages/browsepy/static/
+scp browsepy/templates/*.html root@moddwarf.local:/usr/lib/python3.4/site-packages/browsepy/templates/
+
+ssh root@moddwarf.local "systemctl restart browsepy"
